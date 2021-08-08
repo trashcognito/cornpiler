693d34900e4ad75afb6a173ab89c3ff38ca86c16
kahveciderin <kahveciderin@gmail.com>
2021-08-08 18:48:42 +0300

    added lexer conditions

--- compile.cpp
+++ compile.cpp
@@ -188,7 +188,7 @@ std::vector<token> tokenize_program(char *program, int length) {
   }
 #endif
 
-return status.tokens;
+  return status.tokens;
 }
 
 int main() {
@@ -211,6 +211,110 @@ int main() {
 
   std::vector<token> program_tokens = tokenize_program(file_buffer, is_length);
   delete[] file_buffer;
+
+  ast_body global_scope;
+
+  auto recursive_lex = [&](int old_itt, std::vector<int> scope, int parsing_mode = 0) {  // returns the new itt
+
+    int last_seen_op = -1;
+
+    int itt = old_itt;
+    for (; itt < program_tokens.size(); itt++) {
+      auto look_ahead = [&](int count = 1) {
+        itt += count;
+        if (itt >= is_length) {
+          std::cout << "Error: Premature end-of-file reached" << std::endl;
+          exit(-1);
+        }
+        return program_tokens[itt];
+      };
+
+      auto look_behind = [&](int count = 1) {
+        itt -= count;
+        if (itt < 0) {
+          std::cout << "Error: Premature end-of-file reached" << std::endl;
+          exit(-1);
+        }
+        return program_tokens[itt];
+      };
+      token initial_token = look_ahead();  // get next token
+
+      switch (initial_token.type) {
+        case token_type::identifier: {
+          if(initial_token.value == "if"){
+            // double body statement
+          }else if(initial_token.value == "while" || initial_token.value == "foreach") {
+            // single body statement
+          }else if(initial_token.value == "return" || initial_token.value == "break" || initial_token.value == "continue"){
+            // statement
+          }else if(initial_token.value == "deref" || initial_token.value == "ref"){
+            // varop
+          }else if(initial_token.value == "var" ||initial_token.value == "glvar"){ // TODO: autodetect glvar and deprecate it
+            // vardef
+          }else if(initial_token.value == "ptr" || initial_token.value == "arr" || initial_token.value == "unsigned"){
+            // outer type
+          }else if(initial_token.value == "str" || initial_token.value == "bool" || initial_token.value == "byte" || initial_token.value == "word" || initial_token.value == "int" || initial_token.value == "int64" || initial_token.value == "int128" ){
+            // inner type
+          }else if(initial_token.value == "fun"){
+            // have FUN! jk function
+          }else if(initial_token.value == "ext"){ // todo: autodetect and deprecate
+            // external function
+          }else{
+            look_ahead(1);
+            if(program_tokens[itt].value == "("){
+              // function call
+            }else{
+              // this should be a variable
+              // since we have already incremented the itt, we can check whether it is a definition or a reference right away
+              if(program_tokens[itt].value == "=" || program_tokens[itt].value[1] == '='){
+                // definition
+                token operation = program_tokens[itt];
+                token var_name = program_tokens[itt - 1];
+              }else{
+                // reference
+                itt -= 1;
+              }
+            }
+          }
+          break;
+        }
+        case token_type::number: {
+          // number const
+          break;
+        }
+        case token_type::string: {
+          // string const
+          break;
+        }
+        case token_type::bracket: {
+          // brackets
+          break;
+        }
+        case token_type::sym: {
+          // symbols
+          break;
+        }
+      }
+      if(parsing_mode != 0){
+        if(program_tokens[itt].value == "," || program_tokens[itt].value == "." || program_tokens[itt].value == ")"){ // "." could be unnecessary
+          break;
+        }
+      }
+
+      if(parsing_mode == 2){
+        look_ahead(1);
+        itt -= 1; // check for end of line
+        // check for operations
+      }else if(parsing_mode == 3){
+        if(program_tokens[itt].value == "="){
+          break;
+        }
+      }else if(parsing_mode == 4){
+        break;
+      }
+    }
+    return itt;
+  };
 
   return EXIT_SUCCESS;
 }
