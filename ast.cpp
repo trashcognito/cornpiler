b92c677af0530544a8bd843c91579f8417d3397b
trashcognito <denizmersinlioglu2005@gmail.com>
2021-09-14 22:47:15 +0300

    added complete program printer

--- ast.cpp
+++ ast.cpp
@@ -1300,4 +1300,18 @@ namespace ast {
         this->value->print_val(stream);
         stream << ",\n\"constant\": " << (this->constant ? "true" : "false") << "}";   //,"constant": {CONSTANT} }
     }
+    void print_program_to(std::vector<GlobalEntry *> &program, std::stringstream &stream) {
+        stream << "[";
+        bool is_first_print = true;
+        for (auto entry : program) {
+            if (is_first_print) {
+                //omit first comma
+                is_first_print = false;
+            } else {
+                stream << ",";
+            }
+            entry->print_global(stream);
+        }
+        stream << "]";
+    }
 }
